c3108574cb637b928508312fa504745e9bc7411e
vinod <balinavinodkumar626@gmail.com>
2021-02-01 00:13:13 +0530

    add mongoose

--- src/app.js
+++ src/app.js
@@ -1,25 +1,52 @@
-const {MongoClient,ObjectID}=require('mongodb')
-const url="mongodb://localhost:27017"
-const database="family"
-MongoClient.connect(url,(err,client)=>{
-    if(err){
-        console.log('unable to connet')
-    }
-    const db=client.db(database)
-    // db.collection('family').insertOne({
-    //     name:'vinod',
-    //     brother:'manoj'
-    // },(err,result)=>{
-    //     if(err){
-    //         console.log(err)
-    //     }
-    //     console.log(result.ops[0].name)
-    // })
+// const url="mongodb://localhost:27017"
+// const {MongoClient,ObjectID}=require('mongodb')
+
+// const database="family"
+// MongoClient.connect(url,(err,client)=>{
+//     if(err){
+//         console.log('unable to connet')
+//     }
+//     const db=client.db(database)
+//     // db.collection('family').insertOne({
+//     //     name:'vinod',
+//     //     brother:'manoj'
+//     // },(err,result)=>{
+//     //     if(err){
+//     //         console.log(err)
+//     //     }
+//     //     console.log(result.ops[0].name)
+//     // })
+// })
+// const id=new ObjectID()
+// console.log(id)
+const mongoose=require('mongoose')
+const url="mongodb://localhost:27017/family-api"
+mongoose.connect(url)
+const schema=mongoose.Schema({
+    name:String,
+    father:String,
+    mother:String,
+    brother:String
 })
-const id=new ObjectID()
-console.log(id.)
+const family=mongoose.model('Family',schema)
+// const fa=new family({name:"vinod",
+//     father:"ramakrishna",
+//     mother:"sujatha",
+//     brother:"manoj"
+// })
 
-
+family.insertMany([{name:"nagu",
+father:"srinu",
+mother:"pedhamma",
+brother:"manoj"
+},{name:"hari",
+father:"nagesh",
+mother:"barathi",
+brother:"manoj"
+}])
+// fa.save().then((res)=>{
+//     console.log(res)
+// })
 const path=require('path')
 const express=require('express')
 const app=express()
@@ -51,19 +78,3 @@ app.listen(3000,()=>{
 })
 
 
-
-const doPromise=new Promise(data,(resolve,reject)=>{
-    const data=data
-    setTimeout(() => {
-        resolve(data)
-        resolve('vinod')
-        reject('erroeeeeeeeeeeee')
-
-    }, 2000);
-})
-
-doPromise('dfgsfgsg').then((result)=>{
-    console.log(result)
-}).catch((err)=>{
-    console.log(err)
-})
